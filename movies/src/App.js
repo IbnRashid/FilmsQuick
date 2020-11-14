7b38d1d26ff9e2a1ffb096c6a4d32f56560e083f
Tayyeb Rashid <tayyebrashidpro@gmail.com>
2020-11-14 09:24:11 +0400

    fixed css and added search bar in react

--- movies/src/App.js
+++ movies/src/App.js
@@ -9,6 +9,7 @@ const SEARCHAPI =
 
 function App() {
 	const [movies, setMovies] = useState([]);
+	const [searchTerm, setSearchTerm] = useState("");
 
 	useEffect(() => {
 		fetch(APIURL)
@@ -16,12 +17,43 @@ function App() {
 			.then((data) => {
 				setMovies(data.results);
 			});
-	});
+	}, []);
+
+	const handleOnSubmit = (e) => {
+		e.preventDefault();
+		if (searchTerm) {
+			fetch(SEARCHAPI + searchTerm)
+				.then((res) => res.json())
+				.then((data) => {
+					setMovies(data.results);
+				});
+
+			setSearchTerm("");
+		}
+	};
+
+	const handleOnChange = (e) => {
+		setSearchTerm(e.target.value);
+	};
+
 	return (
-		<div className="movie-container">
-			{movies.length > 0 &&
-				movies.map((movie) => <Movie key={movie.id} {...movie} />)}
-		</div>
+		<>
+			<header>
+				<form onSubmit={handleOnSubmit}>
+					<input
+						className="search"
+						type="search"
+						placeholder="Search..."
+						value={searchTerm}
+						onChange={handleOnChange}
+					/>
+				</form>
+			</header>
+			<div className="movie-container">
+				{movies.length > 0 &&
+					movies.map((movie) => <Movie key={movie.id} {...movie} />)}
+			</div>
+		</>
 	);
 }
 
--- movies/src/components/Movie.js
+++ movies/src/components/Movie.js
@@ -2,18 +2,37 @@ import React from "react";
 
 const IMGPATH = "https://image.tmdb.org/t/p/w1280";
 
+const setVoteClass = (vote) => {
+	if (vote >= 8) {
+		return "green";
+	} else if (vote >= 6) {
+		return "orange";
+	} else {
+		return "red";
+	}
+};
+
 const Movie = ({ title, poster_path, overview, vote_average }) => (
 	<div className="movie">
-		<img src={IMGPATH + poster_path} alt={title} />
-        <div className="movie-info">
-            <h3>{title}</h3>
-            <span>{vote_average}</span>
-        </div>
+		<img
+			src={
+				poster_path
+					? IMGPATH + poster_path
+					: "https://images.pexels.com/photos/2873486/pexels-photo-2873486.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+			}
+			alt={title}
+		/>
+		<div className="movie-info">
+			<h3>{title}</h3>
+			<span className={`tag ${setVoteClass(vote_average)}`}>
+				{vote_average}
+			</span>
+		</div>
 
-        <div className="movie-over">
-            <h2>Overview:</h2>
-            <p>{overview}</p>
-        </div>
+		<div className="movie-over">
+			<h2>Overview:</h2>
+			<p>{overview}</p>
+		</div>
 	</div>
 );
 
